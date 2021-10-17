c576cdd093b061ae12a8b79bf485f6f0715670bc
ushanov.alexander <ushanov.alexander@tyrell.group>
2021-10-17 14:55:52 +0300

    custom.el

--- custom.el
+++ custom.el
@@ -30,10 +30,12 @@
      ("melpa" . "https://melpa.org/packages/")
      ("melpa-stable" . "https://stable-melpa.org/packages/")
      ("melpa" . "https://melpa.org/packages/")
+     ("melpa-stable" . "https://stable-melpa.org/packages/")
+     ("melpa" . "https://melpa.org/packages/")
      ("melpa-stable" . "https://stable-melpa.org/packages/")))
  '(package-enable-at-startup nil)
  '(package-selected-packages
-   '(diff-hl-mode diff-hl doom-modeline adaptive-wrap evil-multiedit memory-usage goto-last-change highlight highlight-symbol default-text-scale alchemist elixir-mode yasnippet-snippets org-duration yaml-mode which-key use-package-ensure-system-package undo-tree smex shackle rainbow-mode rainbow-identifiers rainbow-delimiters quelpa-use-package org-pomodoro mood-line magit lsp-mode lispyville idle-highlight-mode highlight-sexp git-auto-commit-mode general gcmh flycheck-projectile flycheck-clj-kondo evil-org evil-lispy evil-commentary evil-collection darktooth-theme counsel-projectile company clojure-snippets clj-refactor centered-cursor-mode beancount auto-package-update anakondo))
+   '(git-timemachine diff-hl-mode diff-hl doom-modeline adaptive-wrap evil-multiedit memory-usage goto-last-change highlight highlight-symbol default-text-scale alchemist elixir-mode yasnippet-snippets org-duration yaml-mode which-key use-package-ensure-system-package undo-tree smex shackle rainbow-mode rainbow-identifiers rainbow-delimiters quelpa-use-package org-pomodoro mood-line magit lsp-mode lispyville idle-highlight-mode highlight-sexp git-auto-commit-mode general gcmh flycheck-projectile flycheck-clj-kondo evil-org evil-lispy evil-commentary evil-collection darktooth-theme counsel-projectile company clojure-snippets clj-refactor centered-cursor-mode beancount auto-package-update anakondo))
  '(safe-local-variable-values
    '((org-todo-keywords quote
 			((sequence "TODO(t)" "WAIT(w)" "RVIW(r)" "STRT(s)" "CTRL(c)" "HOLD(h)" "TEST(q)" "|" "DONE(d)" "KILL(k)"))))))
@@ -42,4 +44,6 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(diff-hl-change ((t (:background "#e7db74" :foreground "#e7db74"))))
+ '(diff-hl-delete ((t (:background "#f83535" :foreground "#f83535"))))
+ '(diff-hl-insert ((t (:background "#a6e22c" :foreground "#a6e22c")))))
