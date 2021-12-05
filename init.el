29ac2b0cbde311a6d2def8b8866bfd485901f755
ushanov.alexander <ushanov.alexander@tyrell.group>
2021-12-05 17:10:37 +0300

    init.el

--- init.el
+++ init.el
@@ -949,7 +949,7 @@ WARNING: this is a simple implementation. The chance of generating the same UUID
   (flawless-mode-def
     :infix "j"
     :keymaps 'clojure-mode-map
-    "j" 'cider-jack-in)
+    "c" 'cider-jack-in)
   (flawless-mode-def
     :infix "c"
     :keymaps 'clojure-mode-map
