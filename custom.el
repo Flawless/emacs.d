939927ee302768138a9c35d3fc07b99c9aa12620
ushanov.alexander <ushanov.alexander@tyrell.group>
2021-12-16 15:21:09 +0300

    custom.el

--- custom.el
+++ custom.el
@@ -58,6 +58,8 @@
      ("melpa" . "https://melpa.org/packages/")
      ("melpa-stable" . "https://stable.melpa.org/packages/")
      ("melpa" . "https://melpa.org/packages/")
+     ("melpa-stable" . "https://stable.melpa.org/packages/")
+     ("melpa" . "https://melpa.org/packages/")
      ("melpa-stable" . "https://stable.melpa.org/packages/")))
  '(package-enable-at-startup nil)
  '(package-selected-packages
