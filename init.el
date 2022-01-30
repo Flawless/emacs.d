18a6617b86216afd668dfb09ada74500fac9eb41
ushanov.alexander <ushanov.alexander@tyrell.group>
2022-01-31 01:09:35 +0300

    init.el

--- init.el
+++ init.el
@@ -865,6 +865,22 @@ my-org-clocktable-formatter' to that clocktable's arguments."
     (reg-modal 1)
     (attempt-all 1)
     (try-all 1))
+  (define-clojure-indent
+    (re-frame.core/reg-event-fx 1)
+    (re-frame.core/reg-fx 1)
+    (rf/reg-event-fx 1)
+    (rf/reg-fx 1)
+    (re-frame.core/reg-event-db 1)
+    (re-frame.core/reg-db 1)
+    (rf/reg-event-db 1)
+    (rf/reg-db 1)
+    (re-frame.core/reg-sub 1)
+    (rf/reg-sub 1)
+    (component-style-def 1)
+    (reg-view 1)
+    (reg-modal 1)
+    (attempt-all 1)
+    (try-all 1))
   (defun +/insert-random-uuid ()
     "Insert a random UUID.
 Example of a UUID: 1df63142-a513-c850-31a3-535fc3520c3d
