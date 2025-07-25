4f97c1eef8f894b9a9f7e10d61a43e1e3ea4abc2
flawless <alushanov92@gmail.com>
2025-07-25 13:35:24 +0400

    Use upstream straight.el instead of fork

--- init.el
+++ init.el
@@ -36,7 +36,7 @@
   (unless (file-exists-p bootstrap-file)
     (with-current-buffer
       (url-retrieve-synchronously
-        "https://raw.githubusercontent.com/flawless/straight.el/master/install.el"
+        "https://raw.githubusercontent.com/radian-software/straight.el/develop/install.el"
         'silent
         'inhibit-cookies)
       (goto-char (point-max))
